484139cedea4f2cf0ab74e7be379ba795c193ff8
KBRR11 <kenyramirez@upeu.edu.pe>
2020-04-23 15:54:35 -0500

    first commit

---
+++ Hola1/src/hola1/hola1.java
@@ -0,0 +1,50 @@
+package hola1;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class hola1 {
+	
+	public static void main(String[] args) {
+		ServerSocket server;
+		Socket connection;
+		
+		DataOutputStream output;
+		BufferedInputStream bis;
+		BufferedOutputStream bos;
+		
+		byte[] receivedData;
+		int in;
+		String file;
+		
+		try {
+			server = new ServerSocket( 5000 );
+			while (true) {
+				connection = server.accept();
+				receivedData = new byte[1024];
+				bis = new BufferedInputStream(connection.getInputStream());
+				DataInputStream dis = new DataInputStream(connection.getInputStream());
+				
+				file = dis.readUTF();
+				file = file.substring(file.indexOf('\\')+1,file.length());
+					
+				bos = new BufferedOutputStream(new FileOutputStream(file));
+				while ((in = bis.read(receivedData)) != -1) {
+					bos.write(receivedData,0,in);
+				}
+				bos.close();
+				dis.close();
+			}
+		}catch (Exception e) {
+			
+			System.err.print(e);
+		}
+
+	}
+
+}
---
+++ Hola2/src/hola2/hola2.java
@@ -0,0 +1,41 @@
+package hola2;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.Socket;
+
+public class hola2 {
+
+	public static void main(String[] args) {
+		DataInputStream input;
+		BufferedInputStream bis;
+		BufferedOutputStream bos;
+		int in;
+		byte[] byteArray;
+		
+		final String filename ="C:\\Users\\anato\\OneDrive\\Escritorio\\DROP_AGRI.txt";
+		
+		try {
+			final File localFile = new File( filename);
+			Socket client = new Socket("localhost", 5000);
+			bis = new BufferedInputStream(new FileInputStream(localFile));
+			bos = new BufferedOutputStream(client.getOutputStream());
+			
+			DataOutputStream dos= new DataOutputStream(client.getOutputStream());
+			dos.writeUTF(localFile.getName());
+			
+			byteArray = new byte[8192];
+			while ((in = bis.read(byteArray)) != -1) {
+				bos.write(byteArray,0,in);
+			}
+			bis.close();
+			bos.close();
+		}catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+}
